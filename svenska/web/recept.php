14c739eb4d7cb16ca2ee7a483f828da51ec08913
Andreas Remar <andreas.remar@gmail.com>
2010-03-03 21:29:28 +0100

    En del nya recept, och lite andra småändringar.

--- svenska/web/recept.php
+++ svenska/web/recept.php
@@ -18,13 +18,34 @@
 <h2>Recept</h2>
 
 <p>
-Massor med SCD-recept på svenska!! Wooohooo!!
+Varsågod, här finner du SCD-recept. Jag har inte personligen tillagat alla
+recept här, så jag kan inte garantera att det blir gott. :)
 </p>
 
 <table>
 
 <tr><td class="heading">Huvudrätter</td></tr>
 <tr><td><a href="visa.php?recept=moussaka.xml">Moussaka</a></td></tr>
+<tr><td><a href="visa.php?recept=flaskpannkaka.xml">Fläskpannkaka</a></td></tr>
+<tr><td><a href="visa.php?recept=kottfarslimpa.xml">Köttfärslimpa med grönsaker</a></td></tr>
+
+<tr><td class="heading">Soppor</td></tr>
+<tr><td><a href="visa.php?recept=morotssoppa.xml">Morotssoppa</a></td></tr>
+<tr><td><a href="visa.php?recept=tomatsoppa.xml">Tomatsoppa</a></td></tr>
+<tr><td><a href="visa.php?recept=gazpachosoppa.xml">Gazpachosoppa</a></td></tr>
+<tr><td><a href="visa.php?recept=gronsakssoppa.xml">Hjärtlig grönsakssoppa</a></td></tr>
+<tr><td><a href="visa.php?recept=kycklingsoppa.xml">Kycklingsoppa</a></td></tr>
+<tr><td><a href="visa.php?recept=aggplantssoppa.xml">Äggplantssoppa</a></td></tr>
+
+<tr><td class="heading">Tillbehör</td></tr>
+<tr><td><a href="visa.php?recept=persiljesmor.xml">Persiljesmör</a></td></tr>
+<tr><td><a href="visa.php?recept=jordnotssmorrora.xml">Jordnötssmörröra</a></td></tr>
+<tr><td><a href="visa.php?recept=leverpate.xml">Leverpaté</a></td></tr>
+<tr><td><a href="visa.php?recept=ostdipp.xml">Ostdipp</a></td></tr>
+<tr><td><a href="visa.php?recept=chilisas.xml">Chilisås</a></td></tr>
+<tr><td><a href="visa.php?recept=honungschutney.xml">Honungschutney</a></td></tr>
+<tr><td><a href="visa.php?recept=ketchup.xml">Ketchup</a></td></tr>
+<tr><td><a href="visa.php?recept=majonas.xml">Majonäs</a></td></tr>
 
 <tr><td class="heading">Efterrätter</td></tr>
 <tr><td><a href="visa.php?recept=stekt-apple.xml">Stekt äpple med creme fraice</a></tr></td>
